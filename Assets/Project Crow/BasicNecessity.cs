deb3caa97b2fb76c935793c78fb672ba3b609933
Arnarck <andreluismolcalais@gmail.com>
2022-06-19 17:06:36 -0300

    Update BasicNecessity.cs
    Added a feature that, when the player runs out of necessity (hunger and thirst), his max health and max stamina will be limited to half.

--- Assets/Project Crow/BasicNecessity.cs
+++ Assets/Project Crow/BasicNecessity.cs
@@ -1,16 +1,25 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
 
+enum NecessityType
+{
+    Hunger,
+    Thirst
+}
+
 public abstract class BasicNecessity : MonoBehaviour
 {
     float _currentValue, _recoverMultiplier, _timeElapsed;
     bool _hasValue = true;
 
+    [SerializeField] NecessityType type;
     [SerializeField] Slider bar;
     [SerializeField] float timeToDecay = 10f;
     [SerializeField] [Range(0f, 100f)] float startValue = 100f;
     [SerializeField] [Range(0f, 100f)] float maxValue = 100f;
     [SerializeField] [Range(0f, 10f)] float decayValue = 5f;
+    [Tooltip("Limit the max health / stamina")]
+    [SerializeField] [Range(0f, 1f)] float statusLimiter;
 
     public float CurrentValue { get => _currentValue; }
     public float RecoverMultiplier { get => _recoverMultiplier; }
@@ -40,8 +49,20 @@ public abstract class BasicNecessity : MonoBehaviour
     {
         if (_currentValue >= 75f) _recoverMultiplier = 1.25f;
         else if (_currentValue >= 40f) _recoverMultiplier = 1f;
-        else if (_currentValue >= 20f) _recoverMultiplier = .75f;
-        else _recoverMultiplier = .5f;
+        else if (_currentValue >= 20f) _recoverMultiplier = 0.75f;
+        else _recoverMultiplier = 0.5f;
+
+        // Limits the max health / stamina if _currentValue is 0. If not, sets the base value.
+        if (_currentValue < Mathf.Epsilon)
+        {
+            if (type.Equals(NecessityType.Hunger)) GI.Instance.player.ClampMaxHealth(GI.Instance.player.MaxHealth * statusLimiter);
+            else GI.Instance.player.ClampMaxStamina(GI.Instance.player.MaxStamina * statusLimiter);
+        }
+        else
+        {
+            if (type.Equals(NecessityType.Hunger)) GI.Instance.player.ClampMaxHealth(GI.Instance.player.MaxHealth);
+            else GI.Instance.player.ClampMaxStamina(GI.Instance.player.MaxStamina);
+        }
     }
 
     void DecayValue()
--- Assets/Project Crow/Player/Player.cs
+++ Assets/Project Crow/Player/Player.cs
@@ -7,6 +7,7 @@ public class Player : MonoBehaviour
     Coroutine _refillStamina;
     bool _isAlive = true, _canRun = true;
     float _currentHealth, _currentStamina;
+    float _currentMaxHealth, _currentMaxStamina;
 
     [Header("Health")]
     [SerializeField] Slider healthBar;
@@ -23,9 +24,14 @@ public class Player : MonoBehaviour
 
     public bool CanRun { get => _canRun; }
     public bool IsAlive { get => _isAlive; }
+    public float MaxHealth { get => maxHealth; }
+    public float MaxStamina { get => maxStamina; }
 
     void Start()
     {
+        _currentMaxHealth = maxHealth;
+        _currentMaxStamina = maxStamina;
+
         _currentHealth = startHealth;
         healthBar.value = startHealth;
         healthBar.maxValue = maxHealth;
@@ -35,12 +41,36 @@ public class Player : MonoBehaviour
         staminaBar.maxValue = maxStamina;
     }
 
+    public void ClampMaxHealth(float value)
+    {
+        value = Mathf.Clamp(value, 0f, maxHealth);
+        _currentMaxHealth = value;
+
+        if (_currentHealth > value)
+        {
+            _currentHealth = value;
+            healthBar.value = value;
+        }
+    }
+
+    public void ClampMaxStamina(float value)
+    {
+        value = Mathf.Clamp(value, 0f, maxStamina);
+        _currentMaxStamina = value;
+
+        if (_currentStamina > value)
+        {
+            _currentStamina = value;
+            staminaBar.value = value;
+        }
+    }
+
     public void ModifyHealthAmount(float value)
     {
-        if (!_isAlive || _currentHealth >= maxHealth) return;
+        if (!_isAlive || _currentHealth >= _currentMaxHealth) return;
 
         _currentHealth += value;
-        _currentHealth = Mathf.Clamp(_currentHealth, 0f, maxHealth);
+        _currentHealth = Mathf.Clamp(_currentHealth, 0f, _currentMaxHealth);
         healthBar.value = _currentHealth;
 
         if (_currentHealth < Mathf.Epsilon)
@@ -56,7 +86,7 @@ public class Player : MonoBehaviour
         if (!_canRun) return;
 
         _currentStamina -= value;
-        _currentStamina = Mathf.Clamp(_currentStamina, 0, maxStamina);
+        _currentStamina = Mathf.Clamp(_currentStamina, 0, _currentMaxStamina);
         staminaBar.value = _currentStamina;
 
         if (_refillStamina != null) StopCoroutine(_refillStamina);
@@ -66,11 +96,11 @@ public class Player : MonoBehaviour
 
     public void IncreaseStamina(float value)
     {
-        if (_currentStamina >= maxStamina) return;
+        if (_currentStamina >= _currentMaxStamina) return;
 
         value = Mathf.Abs(value);
         _currentStamina += value;
-        _currentStamina = Mathf.Clamp(_currentStamina, 0, maxStamina);
+        _currentStamina = Mathf.Clamp(_currentStamina, 0, _currentMaxStamina);
         staminaBar.value = _currentStamina;
 
         if (!_canRun && _currentStamina >= minStaminaToRun) _canRun = true;
@@ -96,6 +126,7 @@ public class Player : MonoBehaviour
         // Refill stamina over the time.
         while (_currentStamina < maxStamina)
         {
+            Debug.Log(staminaFilledPerFrame * GI.Instance.thirst.RecoverMultiplier);
             IncreaseStamina(staminaFilledPerFrame * GI.Instance.thirst.RecoverMultiplier);
             yield return Util.Instance.waitForEndOfFrame;
         }
