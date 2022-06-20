487b90568acf016ed190f19a19b50b03a95731d3
Arnarck <andreluismolcalais@gmail.com>
2022-06-20 11:41:43 -0300

    Add Inventory System

---
+++ Assets/Project Crow/Player/PlayerInventory.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+public class PlayerInventory : MonoBehaviour
+{
+    int[] m_items_stored = new int[System.Enum.GetValues(typeof(ConsumableType)).Length];
+    int[] m_max_items_stored = { 10, 10, 10, 10 };
+
+    public void Store(ConsumableType consumable, int amount)
+    {
+        int item = (int)consumable;
+
+        if (m_items_stored[item] >= m_max_items_stored[item]) return;
+        if (amount < 0) amount *= -1;
+
+        m_items_stored[item] += amount;
+        m_items_stored[item] = Mathf.Clamp(m_items_stored[item], 0, m_max_items_stored[item]);
+    }
+
+    public void Remove(ConsumableType consumable, int amount)
+    {
+        int item = (int)consumable;
+
+        if (m_items_stored[item] < 1) return;
+        if (amount < 0) amount *= -1;
+
+        m_items_stored[item] -= amount;
+        m_items_stored[item] = Mathf.Clamp(m_items_stored[item], 0, m_max_items_stored[item]);
+    }
+}
