7cbc6426b74472270ccd9b4e24f59c9044510cf1
Arnarck <andreluismolcalais@gmail.com>
2022-11-03 12:01:52 -0300

    Added Enemy Trigger; Improved Level Design;
    Enemy Trigger will make all enemies inside an array start chasing the player when he collides with a trigger;

---
+++ Assets/Project Crow/Enemy/EnemyTrigger.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class EnemyTrigger : MonoBehaviour
+{
+    public EnemyAI[] enemies_to_provoke;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            foreach (EnemyAI enemy in enemies_to_provoke)
+            {
+                if (enemy.is_alive) enemy.is_provoked = true;
+            }
+        }
+    }
+}
