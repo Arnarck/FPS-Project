56d1192441fc4e0b1a28e52ccd655cdb308415b9
Arnarck <andreluismolcalais@gmail.com>
2022-12-07 19:41:09 -0300

    Improved Level Design.

--- Assets/Project Crow/Player/InventoryData.cs
+++ Assets/Project Crow/Player/InventoryData.cs
@@ -1,9 +1,9 @@
 ﻿
 
-// @Arnarck move this to GI
+// @TODO: Put this on an gameobject so i don't need to open VS to change this
 public static class InventoryData
 {
     public static string[] name = { };
     public static string[] description = { };
-    public static int[] max_capacity = { 0, 1, 1, 1, 1, 1, 30, 16, 60, 90, 1, 1, 1, 1, 5, 0 };
+    public static int[] max_capacity = { 0, 1, 1, 1, 1, 1, 40, 16, 60, 90, 1, 1, 1, 1, 5, 0 };
 }
