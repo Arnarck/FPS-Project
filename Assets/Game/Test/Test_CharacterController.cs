f0343eac2c4b18e86622d2d91a62ab0a8638c385
Arnarck <andreluismolcalais@gmail.com>
2023-02-14 21:16:34 -0300

    Improved Level Design (And started some movement tests)

---
+++ Assets/Game/Test/Test_CharacterController.cs
@@ -0,0 +1,110 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test_CharacterController : MonoBehaviour
+{
+    [HideInInspector] public Vector3 fp_camera_start_rotation;
+    [HideInInspector] public Vector3 camera_rotation;
+    [HideInInspector] public Vector3 dp;
+
+
+    public Test_CrossPlatformInput input;
+
+    [Header("Movement")]
+    public float friction = 15f;
+    public float acceleration = 100f;
+
+    [Header("Camera")]
+    public Transform fp_camera;
+    public float mouse_sensitivity = 2f;
+    public float camera_min_yall = -60f;
+    public float camera_max_yall = 60f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        fp_camera_start_rotation = fp_camera.localEulerAngles;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        Vector3 ddp = Vector3.zero;
+        float fdt = Time.fixedDeltaTime;
+
+        { // Character Movement
+            ddp += input.movement_direction();
+        }
+
+        { // Gravity
+            //ddp.y -= -.3f;
+        }
+
+        { // Apply movement
+            ddp *= acceleration;
+            ddp -= dp * friction;
+
+
+            /*
+             a = acceleration
+             v = final velocity
+             u = initial velocity
+             t = time taken
+             s = displacement
+             */
+
+            // https://spm-physics-402.blogspot.com/2009/01/acceleration.html
+
+            // v = u + at
+            // dp = dp + ddp * fdt
+            // "dp" is both "initial velocity (u)" and "final velocity (v)"
+            dp += ddp * fdt;
+            
+            // s = ut + 1/2at^2
+            // s = (u * t) + (1/2 * a * t^2)
+            // transform.position = transform.position + s ("s" is the result of the formula above. The displacement)
+            transform.position += (dp * fdt) + (ddp * fdt * fdt * 0.5f);
+        }
+    }
+
+    private void Update()
+    {
+        { // Camera Movement
+            camera_rotation += input.camera_look_direction() * mouse_sensitivity;
+            Debug.Log(input.camera_look_direction());
+
+            // Clamp rotation
+            camera_rotation.x = Mathf.Clamp(camera_rotation.x, camera_min_yall, camera_max_yall);
+            if (camera_rotation.y < 0f) camera_rotation.y += 360f;
+            else if (camera_rotation.y >= 360f) camera_rotation.y -= 360f;
+
+            fp_camera.localRotation = Quaternion.Euler(fp_camera_start_rotation + camera_rotation);
+        }
+    }
+}
+
+
+//ddp *= acceleration;
+//ddp -= dp * friction;
+
+
+///*
+// a = acceleration
+// v = final velocity
+// u = initial velocity
+// t = time taken
+// s = displacement
+// */
+
+//// https://spm-physics-402.blogspot.com/2009/01/acceleration.html
+
+//// v = u + at
+//// dp = dp + ddp * fdt
+//// "dp" is both "initial velocity (u)" and "final velocity (v)"
+//dp += ddp * fdt;
+
+//// s = ut + 1/2at^2
+//// s = (u * t) + (1/2 * a * t^2)
+//// transform.position = transform.position + s ("s" is the result of the formula above. The displacement)
+//transform.position += (dp * fdt) + (ddp * fdt * fdt * 0.5f);
---
+++ Assets/Game/Test/Test_CrossPlatformInput.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test_CrossPlatformInput : MonoBehaviour
+{
+    public KeyCode move_forward_key;
+    public KeyCode move_backward_key;
+    public KeyCode move_left_key;
+    public KeyCode move_right_key;
+
+    public Vector3 movement_direction()
+    {
+        Vector3 direction = Vector3.zero;
+
+        if (Input.GetKey(move_forward_key)) direction.z += 1f;
+        if (Input.GetKey(move_backward_key)) direction.z -= 1f;
+        if (Input.GetKey(move_left_key)) direction.x -= 1f;
+        if (Input.GetKey(move_right_key)) direction.x += 1f;
+
+        return direction.normalized;
+    }
+
+    public bool moving()
+    {
+        return movement_direction().magnitude > 0f;
+    }
+
+    public Vector3 camera_look_direction()
+    {
+        Vector3 direction = Vector3.zero;
+
+        float pitch = Input.GetAxis("Mouse Y"); // Y axis displacement rotates around the X axis
+        float yall = Input.GetAxis("Mouse X"); // X axis displacement rotates around the Y axis
+
+        direction.x = -pitch;
+        direction.y = yall;
+
+        return direction;
+    }
+
+    public bool rotating_camera()
+    {
+        return camera_look_direction().magnitude > 0f;
+    }
+}
