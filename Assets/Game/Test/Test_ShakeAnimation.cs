d5e8c11eefc8a211272f77616658a0a313065c8a
Arnarck <andreluismolcalais@gmail.com>
2023-03-11 20:23:15 -0300

    Finished Base Shake Animation.

--- Assets/Game/Test/Test_ShakeAnimation.cs
+++ Assets/Game/Test/Test_ShakeAnimation.cs
@@ -2,7 +2,7 @@
 
 
 
-public enum EWaveQuadrant
+public enum ECircleQuadrant
 {
     NONE,
     FIRST,
@@ -23,25 +23,15 @@ public struct FShakeAnimationSettings
 {
     public float duration;
 
-    [Header("X Axis")]
-    public bool x_randomize_start_movement;
-    public float x_amplitude;
-    public float x_frequency;
+    [Header("Directional Shake")]
+    public Vector3 amplitude;
+    public Vector3 frequency;
+    public bool[] randomize;
 
-    [Header("Y Axis")]
-    public bool y_randomize_start_movement;
-    public float y_amplitude;
-    public float y_frequency;
-
-    [Header("Z Axis")]
-    public bool z_randomize_start_movement;
-    public float z_amplitude;
-    public float z_frequency;
-
-    [Header("Rotation")]
-    public bool angular_randomize_start_movement;
-    public float angular_frequency;
+    [Header("Angular Shake")]
+    public Vector3 angular_frequency;
     public Vector3 angular_amplitude;
+    public bool[] randomize_rotation;
 }
 
 
@@ -50,95 +40,110 @@ public class Test_ShakeAnimation : MonoBehaviour
 {
     float duration_t;
     bool shaking;
-    float x_animation_time, y_animation_time, z_animation_time, angular_animation_time;
-    EWaveQuadrant x_animation_on_key_up, y_animation_on_key_up, z_animation_on_key_up, angular_animation_on_key_up;
-    FShakeAnimationSettings current_settings;
+    ECircleQuadrant[] quadrant_on_animation_end = new ECircleQuadrant[3], angular_quadrant_on_animation_end = new ECircleQuadrant[3]; // 0 - x; 1 - y; z - 2
+    Vector3 unit_circle, angular_unit_circle;
+    FShakeAnimationSettings shake;
 
     [HideInInspector] public Vector3 displacement, angular_displacement;
 
-    public FShakeAnimationSettings settings;
+    public FShakeAnimationSettings shake_settings;
 
     private void Update()
     {
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            add_shake();
+            add_shake(shake_settings);
         }
 
         update(Time.deltaTime);
     }
 
-    public void add_shake()
+    public void add_shake(FShakeAnimationSettings settings, bool reset_duration_if_already_shaking = true)
     {
-        duration_t = settings.duration;
-
-        if (!shaking) // Only randomize the direction if the player wasn't previously shaking.
+        if (shaking)
         {
-            current_settings.x_amplitude = settings.x_amplitude;
-            if (settings.x_randomize_start_movement) current_settings.x_amplitude *= Random.Range(0, 2) == 0 ? -1 : 1;
-
-            current_settings.y_amplitude = settings.y_amplitude;
-            if (settings.y_randomize_start_movement) current_settings.y_amplitude *= Random.Range(0, 2) == 0 ? -1 : 1;
-
-            current_settings.z_amplitude = settings.z_amplitude;
-            if (settings.z_randomize_start_movement) current_settings.z_amplitude *= Random.Range(0, 2) == 0 ? -1 : 1;
-
-            current_settings.angular_amplitude.x = settings.angular_amplitude.x;
-            current_settings.angular_amplitude.y = settings.angular_amplitude.y;
-            current_settings.angular_amplitude.z = settings.angular_amplitude.z;
-
-            if (settings.angular_randomize_start_movement)
+            if (reset_duration_if_already_shaking) duration_t = shake.duration;
+        }
+        else // Start shaking
+        {
+            shaking = true;
+            shake = settings;
+            duration_t = shake.duration;
+            for (int i = 0; i < 3; i++) // Only randomize the direction if the player wasn't previously shaking.
             {
-                current_settings.angular_amplitude.x *= Random.Range(0, 2) == 0 ? -1 : 1;
-                current_settings.angular_amplitude.y *= Random.Range(0, 2) == 0 ? -1 : 1;
-                current_settings.angular_amplitude.z *= Random.Range(0, 2) == 0 ? -1 : 1;
+                if (i < shake.randomize.Length && shake.randomize[i]) shake.amplitude[i] *= Random.Range(0, 2) == 0 ? -1 : 1;
+                if (i < shake.randomize_rotation.Length && shake.randomize_rotation[i]) shake.angular_amplitude[i] *= Random.Range(0, 2) == 0 ? -1 : 1;
             }
         }
-
-        shaking = true;
     }
 
     public void update(float dt)
     {
-        { // Handle the animation based on the player's input
-            if (shaking) // Updates the animation over time
+        if (!shaking) return;
+
+        { // Animation
+            if (duration_t > 0f) // Updates the animation over time
             {
-                x_animation_time = increase_animation_time(x_animation_time, settings.x_frequency, dt);
-                y_animation_time = increase_animation_time(y_animation_time, settings.y_frequency, dt);
-                z_animation_time = increase_animation_time(z_animation_time, settings.z_frequency, dt);
-                angular_animation_time = increase_animation_time(angular_animation_time, settings.angular_frequency, dt);
+                unit_circle += dt * shake.frequency;
+                angular_unit_circle += dt * shake.angular_frequency;
+                for (int i = 0; i < 3; i++)
+                {
+                    // On "time == 1f", the wave completes the 360° cicle. The value is being reseted to don't trepass the float number limit.
+                    if (unit_circle[i] >= 1f) unit_circle[i] -= 1f;
+                    if (angular_unit_circle[i] >= 1f) angular_unit_circle[i] -= 1f;
+                }
 
                 duration_t -= dt;
                 if (duration_t <= 0f) 
                 {
-                    shaking = false;
+                    for (int i = 0; i < 3; i++) // Check where the wave cicle is, and set the "shortest return path".
+                    {
+                        quadrant_on_animation_end[i] = get_circle_quadrant(unit_circle[i]);
+                        if (quadrant_on_animation_end[i] == ECircleQuadrant.THIRD) unit_circle[i] = third_quadrant_to_fourth(unit_circle[i]);
+                        else if (quadrant_on_animation_end[i] == ECircleQuadrant.SECOND) unit_circle[i] = second_quadrant_to_first(unit_circle[i]);
 
-                    // Check where the animation is, on the wave cicle, and set the "shortest return path" based on the location.
-                    x_animation_on_key_up = get_animation_quadrant(x_animation_time);
-                    y_animation_on_key_up = get_animation_quadrant(y_animation_time);
-                    z_animation_on_key_up = get_animation_quadrant(z_animation_time);
-                    angular_animation_on_key_up = get_animation_quadrant(angular_animation_time);
-
-                    if (x_animation_on_key_up == EWaveQuadrant.THIRD) x_animation_time = third_quadrant_to_fourth(x_animation_time);
-                    else if (x_animation_on_key_up == EWaveQuadrant.SECOND) x_animation_time = second_quadrant_to_first(x_animation_time);
-
-                    if (y_animation_on_key_up == EWaveQuadrant.THIRD) y_animation_time = third_quadrant_to_fourth(y_animation_time);
-                    else if (y_animation_on_key_up == EWaveQuadrant.SECOND) y_animation_time = second_quadrant_to_first(y_animation_time);
-
-                    if (z_animation_on_key_up == EWaveQuadrant.THIRD) z_animation_time = third_quadrant_to_fourth(z_animation_time);
-                    else if (z_animation_on_key_up == EWaveQuadrant.SECOND) z_animation_time = second_quadrant_to_first(z_animation_time);
-
-                    if (angular_animation_on_key_up == EWaveQuadrant.THIRD) angular_animation_time = third_quadrant_to_fourth(angular_animation_time);
-                    else if (angular_animation_on_key_up == EWaveQuadrant.SECOND) angular_animation_time = second_quadrant_to_first(angular_animation_time);
+                        angular_quadrant_on_animation_end[i] = get_circle_quadrant(angular_unit_circle[i]);
+                        if (angular_quadrant_on_animation_end[i] == ECircleQuadrant.THIRD) angular_unit_circle[i] = third_quadrant_to_fourth(angular_unit_circle[i]);
+                        else if (angular_quadrant_on_animation_end[i] == ECircleQuadrant.SECOND) angular_unit_circle[i] = second_quadrant_to_first(angular_unit_circle[i]);
+                    }
                 }
             }
-            
-            if (!shaking) // Finishes the animation (can happen on the same frame as the animation)
+
+            if (duration_t <= 0f) // Finishes the animation (can happen on the same frame as the animation)
             {
-                if (x_animation_time > 0f) x_animation_time = reset_animation(x_animation_time, settings.x_frequency, dt, x_animation_on_key_up);
-                if (y_animation_time > 0f) y_animation_time = reset_animation(y_animation_time, settings.y_frequency, dt, y_animation_on_key_up);
-                if (z_animation_time > 0f) z_animation_time = reset_animation(z_animation_time, settings.z_frequency, dt, z_animation_on_key_up);
-                if (angular_animation_time > 0f) angular_animation_time = reset_animation(angular_animation_time, settings.angular_frequency, dt, angular_animation_on_key_up);
+                shaking = false;
+                for (int i = 0; i < 3; i++)
+                {
+                    if (unit_circle[i] > 0f && unit_circle[i] < 1f)
+                    {
+                        shaking = true;
+                        if (quadrant_on_animation_end[i] == ECircleQuadrant.THIRD || quadrant_on_animation_end[i] == ECircleQuadrant.FOURTH)
+                        {
+                            unit_circle[i] += dt * shake.frequency[i];
+                            if (unit_circle[i] > 1f) unit_circle[i] = 1f;
+                        }
+                        else
+                        {
+                            unit_circle[i] -= dt * shake.frequency[i];
+                            if (unit_circle[i] < 0f) unit_circle[i] = 0f;
+                        }
+                    }
+
+                    if (angular_unit_circle[i] > 0f && angular_unit_circle[i] < 1f)
+                    {
+                        shaking = true;
+                        if (angular_quadrant_on_animation_end[i] == ECircleQuadrant.THIRD || angular_quadrant_on_animation_end[i] == ECircleQuadrant.FOURTH)
+                        {
+                            angular_unit_circle[i] += dt * shake.angular_frequency[i];
+                            if (angular_unit_circle[i] > 1f) angular_unit_circle[i] = 1f;
+                        }
+                        else
+                        {
+                            angular_unit_circle[i] -= dt * shake.angular_frequency[i];
+                            if (angular_unit_circle[i] < 0f) angular_unit_circle[i] = 0f;
+                        }
+                    }
+                }
             }
         }
 
@@ -149,14 +154,13 @@ public class Test_ShakeAnimation : MonoBehaviour
             // amplitude * Mathf.Sin(2 * Mathf.PI * Time.time * frequency + 0)
             // "0" means the "wave phase". It sets in which quadrant the wave will start.
             // The "wave phase" must be set in radian degrees: 0, PI, PI/2, 3PI/2 or 2PI.
-            displacement.x = current_settings.x_amplitude * Mathf.Sin((2 * Mathf.PI) * x_animation_time + 0f);
-            displacement.y = current_settings.y_amplitude * Mathf.Cos(2f * ((2 * Mathf.PI) * y_animation_time + 0f)) - current_settings.y_amplitude;
-            displacement.z = current_settings.z_amplitude * Mathf.Sin((2 * Mathf.PI) * z_animation_time + 0f);
+            displacement.x = shake.amplitude.x * Mathf.Sin((2 * Mathf.PI) * unit_circle.x + 0f);
+            displacement.y = shake.amplitude.y * Mathf.Cos(2f * ((2 * Mathf.PI) * unit_circle.y + 0f)) - shake.amplitude.y;
+            displacement.z = shake.amplitude.z * Mathf.Sin((2 * Mathf.PI) * unit_circle.z + 0f);
 
-            float raw_angular_displacement = Mathf.Sin((2 * Mathf.PI) * angular_animation_time + 0f);
-            angular_displacement.x = current_settings.angular_amplitude.x * raw_angular_displacement;
-            angular_displacement.y = current_settings.angular_amplitude.y * raw_angular_displacement;
-            angular_displacement.z = current_settings.angular_amplitude.z * raw_angular_displacement;
+            angular_displacement.x = shake.angular_amplitude.x * Mathf.Sin(2f * Mathf.PI * angular_unit_circle.x + 0f);
+            angular_displacement.y = shake.angular_amplitude.y * Mathf.Sin(2f * Mathf.PI * angular_unit_circle.y + 0f);
+            angular_displacement.z = shake.angular_amplitude.z * Mathf.Sin(2f * Mathf.PI * angular_unit_circle.z + 0f);
         }
 
         { // Sets displacement - ONLY FOR TEST
@@ -166,55 +170,28 @@ public class Test_ShakeAnimation : MonoBehaviour
 
     }
 
-    public float increase_animation_time(float time, float frequency, float dt)
+    public ECircleQuadrant get_circle_quadrant(float unit_circle)
     {
-        time += dt * frequency;
-        if (time >= 1f)  // Resets the cicle
-        {
-            time -= 1f; // On "time == 1f", the wave completes the 360° cicle. The value is being reseted to don't trepass the float number limit.
-        }
-
-        return time;
+        if (unit_circle >= .75f) return ECircleQuadrant.FOURTH;
+        else if (unit_circle >= .5f) return ECircleQuadrant.THIRD;
+        else if (unit_circle >= .25f) return ECircleQuadrant.SECOND;
+        else return ECircleQuadrant.FIRST;
     }
 
-    public float reset_animation(float time, float frequency, float dt, EWaveQuadrant quadrant)
+    public float third_quadrant_to_fourth(float unit_circle)
     {
-        if (quadrant == EWaveQuadrant.THIRD || quadrant == EWaveQuadrant.FOURTH)
-        {
-            time += dt * frequency;
-            if (time > 1f) time = 1f;
-        }
-        else
-        {
-            time -= dt * frequency;
-            if (time < 0f) time = 0f;
-        }
+        if (unit_circle < .5f || unit_circle >= .75f) return unit_circle;
 
-        return time;
+        unit_circle = 1f - unit_circle; // Returns the time needed to reset the cicle. Ex: 1f - 0.6f == 0.4f
+        unit_circle += 0.5f; // Returns the time needed to reset the FOURTH quadrant. Ex: 0.4f + 0.5f == 0.9f
+
+        return unit_circle;
     }
 
-    public EWaveQuadrant get_animation_quadrant(float time)
+    public float second_quadrant_to_first(float unit_circle)
     {
-        if (time >= .75f) return EWaveQuadrant.FOURTH;
-        else if (time >= .5f) return EWaveQuadrant.THIRD;
-        else if (time >= .25f) return EWaveQuadrant.SECOND;
-        else return EWaveQuadrant.FIRST;
-    }
+        if (unit_circle < .25f || unit_circle >= .5f) return unit_circle;
 
-    public float third_quadrant_to_fourth(float time)
-    {
-        if (time < .5f || time >= .75f) return time;
-
-        time = 1f - time; // Returns the time needed to reset the cicle. Ex: 1f - 0.6f == 0.4f
-        time += 0.5f; // Returns the time needed to reset the FOURTH quadrant. Ex: 0.4f + 0.5f == 0.9f
-
-        return time;
-    }
-
-    public float second_quadrant_to_first(float time)
-    {
-        if (time < .25f || time >= .5f) return time;
-
-        return 0.5f - time;
+        return 0.5f - unit_circle;
     }
 }
